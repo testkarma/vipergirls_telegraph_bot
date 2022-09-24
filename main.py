b50352b5415e213b15f9e233eda88a7467ede2e0
testkarma <114176035+testkarma@users.noreply.github.com>
2022-09-24 18:22:10 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,80 @@
+import bs4, requests, telebot,os
+from collections import namedtuple
+from telegraph import Telegraph
+from io import BytesIO
+
+def get_threads(f='',prefixid='',newset='',page=1,pp=25,daysprune=0):
+    thread_urls = []
+    url = f'https://vipergirls.to/forumdisplay.php?f={f}&prefixid={prefixid}&newset={newset}&page={page}&pp={pp}&daysprune={daysprune}'
+    sitedata = requests.get(url).text
+    soup = bs4.BeautifulSoup(sitedata,'lxml')
+    for u in soup.find_all('a',class_='lastpostdate'):
+        href = u.get('href')
+        thread_urls.append('https://vipergirls.to/'+href[:href.index('?')])
+    return thread_urls
+
+def get_img_urls(page):
+    pagedata = requests.get(page).text
+    soup = bs4.BeautifulSoup(pagedata,'lxml')
+    posts = soup.find_all('blockquote',attrs={'class':'postcontent restore '})
+    urls={'vipr':[],
+          'imx':[]}
+    for post in posts:
+        for u in post.find_all('img',attrs={'border':'0','alt':''}):
+            if 'vipr' in u.get('src'):
+                urls['vipr'].append(u.get('src').replace('/th/','/i/'))
+            elif 'imx' in u.get('src'):
+                urls['imx'].append(u.get('src').replace('/t/','/i/'))
+    if urls['vipr'] == []:
+        return soup.find('title').string,urls['imx']
+    else:
+        return soup.find('title').string,urls['vipr']
+
+def download_all_imgs(img_urls):
+    for i,url in enumerate(img_urls):
+        img_data = requests.get(url)
+        if img_data.ok:
+            with open(f'{i:03}.jpg','wb+') as img_file:
+                img_file.write(img_data.content)
+        
+def create_page(auth_token,title,img_urls):
+    tg = Telegraph(auth_token)
+    # tg_img_urls = [(tg.upload_file(BytesIO(requests.get(url).content))[0]['src'],url) for url in img_urls]
+    tg_img_urls = []
+    for i,url in enumerate(img_urls):
+        tg_img_urls.append((tg.upload_file(BytesIO(requests.get(url).content))[0]['src'],url))
+        # print(f'{i/len(img_urls)}\r',end='')
+    # content = '<img src=\"https://telegra.ph/'+'\"><img src=\"https://telegra.ph/'.join(tg_img_urls)+'\">'
+    content = ''.join([f'<img src=\"https://telegra.ph/{x}\" alt=\"{y}\">\n' for x,y in tg_img_urls])
+    tgraph_page = tg.create_page(title,html_content=content)
+    return tgraph_page['url']
+
+if __name__ == '__main__':
+    Site = namedtuple('Site','f,prefixid')
+    sites = (Site('304','Vixen_com'),Site('304','Tushy_com'),Site('304','TushyRaw_com'),Site('304','Deeper_com'),Site('304','Blacked_com'),Site('304','BlackedRaw_com'),Site('305','Slayed_com'))
+    
+    auth_token = os.environ['TELEGRAPH_TOKEN']
+    chat_id = os.environ['CHAT_ID']
+    api_key = os.environ['TELEGRAM_API_KEY']
+    
+    sent_threads = []
+    with open('sent.txt','r') as file:
+        sent_threads = [i[:-1] for i in file.readlines()]
+    new_threads = []
+    for site in sites:
+        threads = get_threads(site.f,site.prefixid,newset=1)
+        for i in threads:
+            if i not in sent_threads:
+                new_threads.append(i)
+    
+    if new_threads:
+        print('Found new threads!.\n',*[f'\t- {i}\n' for i in new_threads])
+        bot = telebot.TeleBot(api_key)
+        for thread in new_threads:
+            title, img_urls = get_img_urls(thread)
+            link = create_page(auth_token,title,img_urls)
+            bot.send_message(5015371671,link)
+            with open('sent.txt','a') as file:
+                file.writelines([i+'\n' for i in new_threads])
+    else:
+        print('No new threads found. Closing App')
