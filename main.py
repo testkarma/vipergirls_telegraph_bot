83453cd9ebdb6a7a29adff254fd0e6e7c550bb25
testkarma <114176035+testkarma@users.noreply.github.com>
2022-09-25 13:18:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -75,6 +75,8 @@ if new_threads:
     for thread in new_threads:
         title, img_urls = get_img_urls(thread)
         if img_urls:
+            with open('log.txt') as file:
+                file.write(str(img_urls))
             print('test print : ',img_urls)
             link = create_page(auth_token,title,img_urls)
             bot.send_message(5015371671,link)
