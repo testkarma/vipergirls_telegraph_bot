af2919487f95bb75eb6144a050769278b8b72ae2
testkarma <114176035+testkarma@users.noreply.github.com>
2023-01-20 10:05:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -82,7 +82,8 @@ if new_threads:
         if img_urls:
             link = create_page(auth_token,title,img_urls)
             bot.send_message(chat_id,link)
-            bot.send_message(chat_id,f"[{title.replace('-',r'\-')}]({thread})",parse_mode='MarkdownV2')
+            t = title.replace('- ','')
+            bot.send_message(chat_id,f'[{t}]({thread})',parse_mode='MarkdownV2')
             with open('sent.txt','a') as file:
                 file.writelines(thread+'\n')
 else:
